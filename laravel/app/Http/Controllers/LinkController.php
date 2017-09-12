ed6331367f0419b9e453c51e16c5160561aeb5df
Krzysiek91 <domaradzki91@gmail.com>
2017-09-13 01:37:37 +0200

    making the code more efficiant and easier to read

--- laravel/app/Http/Controllers/LinkController.php
+++ laravel/app/Http/Controllers/LinkController.php
@@ -12,42 +12,38 @@ class LinkController extends Controller {
 
     public function create(Request $request) {
 
-        if (Request::isMethod('post')) {
-            $url = Request::input('url');
-            if (isset($url) === true && filter_var($url, FILTER_VALIDATE_URL)) {
-
-                $hash = null;
-                $exists = Link::where('url', '=', $url);
-
-                if ($exists->count() >= 1) {
-                    $hash = $exists->first()->hash;
-                    return redirect('/')->with('get', '<a href="' . [url('/get')][0] . '?h=' . $hash . '">' . [url('/get')][0] . '?h=' . $hash . '</a>');
-                } else {
-                    $urlDB = Link::create(['url' => $url]);
-                    $idDB = $urlDB->id;
-                    $hash = base_convert($idDB, 10, 36);
-
-                    DB::table('links')
-                            ->where('id', $idDB)
-                            ->update(['hash' => $hash]);
-                }
-
-                if ($hash != null) {
-                    return redirect('/')->with('get', '<a href="' . [url('/get')][0] . '?h=' . $hash . '">' . [url('/get')][0] . '?h=' . $hash . '</a>');
-                }
-            } else {
-                return redirect('/')->with('error', 'You probaly did not enter corret URL');
-            }
+        if (!Request::isMethod('post')) {
+            return;
         }
+            
+            
+        $url = Request::input('url');
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            return redirect('/')->with('error', 'You probaly did not enter corret URL');
+        }
+
+        $link = Link::where('url', '=', $url)->get()->first();
+
+        if (!$link) {
+            // If the link does not exist, create one
+            $link = Link::create(['url' => $url]);
+            $idDB = $link->id;
+            $hash = base_convert($idDB, 10, 36);
+            
+            $link->hash = $hash;
+            $link->save();
+        }
+                
+        $url = url('/' . $link->hash);
+        return redirect('/')->with('get', '<a href="' . $url . '">' . $url . '</a>');
     }
 
-    public function get() {
-        if (isset($_GET['h'])) {
-            $link = DB::table('links')->where('hash', $_GET['h'])->first()->url;
-            return Redirect::to($link);
-        } else {
-            return redirect('/')->with('error', 'Something went wrong, try again');
+    public function get($hash) {        
+        $link = Link::where('hash', $hash)->get()->first();      
+        if ($link == null){
+             return redirect('/')->with('error', 'Coud not find the record in DB');
         }
+        return Redirect::to($link->url);
     }
-
+    
 }
